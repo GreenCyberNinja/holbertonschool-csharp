c67d175b10ed857476671b3644e7da5992bed202
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-14 13:20:30 -0500

    prints array of specified size and returns array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] ans = new int [size];
+        int i;
+        for (i = 0; i < size; i++)
+        {
+            ans[i] = i;
+            Console.Write($"{ans[i]} ");
+        }
+        Console.WriteLine("");
+        return ans;
+    }
+}
