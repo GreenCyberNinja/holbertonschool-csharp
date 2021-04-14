c91ce6d89c49008e9908c2585305f8bfe75bd572
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-13 20:33:35 -0500

    prints the alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char ans = 'a'; ans <= 'z'; ans++){
+                Console.Write($"{ans}");
+            }
+            
+        }
+    }
