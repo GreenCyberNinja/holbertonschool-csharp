9ac40d6f6e9daadcb46ece7a0a835508fdd6e45f
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-13 22:15:58 -0500

    prints the last digit place

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            int ans = number % 10;
+            if (ans < 0)
+                ans *= -1;
+            Console.Write($"{ans}");
+            return ans;
+        }
+    }
+
