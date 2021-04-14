7c306a735a7cd8e527559304d8373df0fb88e2b7
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-14 13:29:34 -0500

    gets the element at specified index

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (array.Length < index || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+        else
+            return array[index];
+    }
+}
