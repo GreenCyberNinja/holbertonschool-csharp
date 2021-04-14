03d84da9efc02558bea7c20725f2756820a333c3
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-13 20:34:28 -0500

    prints the alphabet without q or e

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char ans = 'a'; ans <= 'z'; ans++){
+                if (ans != 'q' && ans != 'e'){
+                    Console.Write($"{ans}");
+                }
+                
+            }
+            
+        }
+    }
