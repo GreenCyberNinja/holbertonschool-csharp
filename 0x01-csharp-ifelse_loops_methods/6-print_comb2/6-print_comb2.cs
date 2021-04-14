6492cf6e6a01fd0738d2e4ae45b8362c15ec87f9
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-13 22:14:23 -0500

    prints all unique combinations of 2 numbers

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 0 ; i < 8 ; i++)
+                for (j = i + 1; j <= 9; j++)
+                    Console.Write($"{i}{j}, ");
+            Console.WriteLine($"89");
+        }
+    }
