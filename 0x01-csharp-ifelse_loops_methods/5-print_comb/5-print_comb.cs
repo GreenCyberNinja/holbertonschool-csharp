3cad8c7e9c94cf7145d78d3f72064de9f4e432a5
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-13 22:13:36 -0500

    prints numbers from 00 to 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0 ; i < 99 ; i++)
+                Console.Write("{0:D2}, ", i);
+            Console.WriteLine("{0:D2}", i);
+        }
+    }
