d0710a23c6cf8e3cc30c7a888a43390a3d158024
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-13 20:17:49 -0500

    takes the last digit and checks if its greater than 5 or less than 6

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lst = number % 10;
+        Console.Write($"the last digit of {number} is");
+        if (lst > 5){
+            Console.WriteLine($" {lst} and is greater than 5");
+        }
+        if (lst < 6 && lst != 0) {
+            Console.WriteLine($" {lst} and is less than 6 and not 0");
+        }
+        if (lst == 0){
+            Console.WriteLine($" {lst} and is zero");
+        }
+    }
+}
