565e867e51d97e023707769adaf5ad4b9e4da896
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-14 20:10:27 -0500

    returns number of keys

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int i = 0;
+        foreach (var key in myDict.Keys)
+        {
+            i++;
+        }
+        return i;
+    }
+}
