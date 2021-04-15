be594d32d75335f8a2b544c5deccd3e23249ee32
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-14 21:30:30 -0500

    gets the best score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int max = 0;
+        string maxkey = "";
+        foreach(KeyValuePair<string, int>keval in myList)
+        {
+            if (max < keval.Value)
+            {
+                max = keval.Value;
+                maxkey = keval.Key;
+            }
+        }
+        return maxkey;
+    }
+}
