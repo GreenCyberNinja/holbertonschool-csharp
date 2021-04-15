a3ec7c89f70e4a81f047cec6ee5377f8faf85f6f
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-14 20:06:12 -0500

    prints a list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        var ans = new List<int>();
+        int i;
+        for (i = 0; i < size; i++)
+        {
+            ans.Add(i);
+            Console.Write($"{ans[i]} ");
+        }
+        Console.WriteLine("");
+        return ans;
+    }
+}
