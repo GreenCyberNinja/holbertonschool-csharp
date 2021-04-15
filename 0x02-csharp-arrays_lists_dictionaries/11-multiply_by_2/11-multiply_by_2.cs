c4ab2d56bc1d5e4cfcb0459e2281c2ad2ca58977
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-14 20:13:08 -0500

    multiplies all values in a dictionary by 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> ans = new Dictionary<string, int>();
+
+        foreach (KeyValuePair<string, int>keval in myDict)
+        {
+            ans[keval.Key] = keval.Value * 2; 
+        }
+        return ans;
+    }
+}
