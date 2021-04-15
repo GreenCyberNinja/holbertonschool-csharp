32469f3ae3fbd2360941b72fb1736ffd113b5231
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-14 20:11:36 -0500

    adds key/value pair to dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        Dictionary<string, string> NewD = myDict;
+        if (NewD.ContainsKey(key))
+        {
+            NewD[key] = value;
+        }
+        else
+        {
+            NewD.Add(key, value);
+        }
+        return NewD;
+    }
+}
