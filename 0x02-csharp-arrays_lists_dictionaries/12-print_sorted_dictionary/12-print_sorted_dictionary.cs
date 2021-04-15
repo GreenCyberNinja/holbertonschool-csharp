56a870c6fbd61cc8c2c52eca74a63913a54ce578
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-14 21:26:32 -0500

    prints a sorted dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        SortedList<string, string> ans = new SortedList<string, string>();
+        foreach (KeyValuePair<string, string>keval in myDict)
+        {
+            ans[keval.Key] = keval.Value;
+        }
+        foreach (var item in ans)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+}
