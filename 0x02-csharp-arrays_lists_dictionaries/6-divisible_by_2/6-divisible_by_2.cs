debba912101c1349a77fbe583428e8a0b788b003
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-14 20:08:14 -0500

    checks if ints in a given list are divisible by 2 or not

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        var ans = new List<bool>();
+        foreach (int num in myList)
+        {
+            if (num % 2 != 0)
+                ans.Add(false);
+            else
+                ans.Add(true);
+        }
+        return ans;
+    }
+}
