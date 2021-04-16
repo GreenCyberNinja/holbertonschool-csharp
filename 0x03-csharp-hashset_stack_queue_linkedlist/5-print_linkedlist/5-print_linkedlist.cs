8d3f69da95dc0a53211f5cd80f16a932b5a2bce4
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-15 23:25:34 -0500

    prints the linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> ans = new LinkedList<int>();
+        for (int ind = 0; ind < size; ind++)
+        {
+            Console.WriteLine($"{ind}");
+            ans.AddFirst(ind);
+        }
+        return ans;
+    }
+}
