75a3b87299cb31f975ca45b9409f8e5581755be1
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-15 20:34:10 -0500

    adds all unique numbers in two lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int ans = 0;
+        List<int> check = new List<int>();
+    
+        foreach (int i in myList)
+        {
+            if (!check.Contains(i))
+            {
+                ans += i;
+                check.Add(i);
+            };
+        }
+        return ans;
+    }
+}
