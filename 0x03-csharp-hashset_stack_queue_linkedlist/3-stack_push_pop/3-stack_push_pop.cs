1e34a24cf1f6482ad3ea5475a07f4160e1aaee1f
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-15 21:04:30 -0500

    manipulates a given stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        bool con = aStack.Contains(search);
+        string[] ans = aStack.ToArray();
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        Console.WriteLine($"Top item: {aStack.Peek()}");
+        Console.WriteLine($"Stack contains {search}: {con}");
+        if (con)
+        {
+            foreach (string i in ans)
+            {
+                aStack.Pop();
+                if (i == search)
+                    break;
+            }
+            aStack.Push(newItem);
+        }
+        return aStack;
+
+    }
+}
