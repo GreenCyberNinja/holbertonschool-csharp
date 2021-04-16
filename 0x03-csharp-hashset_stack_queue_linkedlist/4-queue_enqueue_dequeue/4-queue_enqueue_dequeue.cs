42bf21feee9a8b864b49c1c38fde9b18dfebcfb1
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-15 21:21:41 -0500

    manipulates a queue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        bool con = aQueue.Contains(search);
+        string[] ans = aQueue.ToArray();
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+        Console.WriteLine($"Top item: {aQueue.Peek()}");
+        Console.WriteLine($"Stack contains {search}: {con}");
+        if (con)
+        {
+            foreach (string i in ans)
+            {
+                aQueue.Dequeue();
+                if (i == search)
+                    break;
+            }
+            aQueue.Enqueue(newItem);
+        }
+        return aQueue;
+
+    }
+}
