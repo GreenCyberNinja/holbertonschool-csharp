0c3531413c4f98fb6a91c456141d1490c16c1b2b
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-15 23:29:32 -0500

    removes node at begining of list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int ans;
+        
+        if (myLList.Any())
+        {
+            ans = myLList.First.Value;
+            myLList.RemoveFirst();
+        }
+        else
+            ans = 0;
+        return ans;
+    }
+}
