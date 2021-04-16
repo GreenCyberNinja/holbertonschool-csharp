7f5793ca0bb8db18193fce360f64da125871e418
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-15 20:41:40 -0500

    returns all commonm elements between 2 lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> ans = new List<int>();
+        foreach (int i in list1)
+        {
+            if (list2.Contains(i))
+            {
+                ans.Add(i);
+            }
+        }
+        ans.Sort();
+        return ans;
+    }
+}
