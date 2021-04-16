1576a6f1026a8101b068a3b8607b4b1bf31bdc39
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-15 23:30:11 -0500

    gets a node by index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int i = 0;
+        LinkedListNode<int> nav = myLList.First;
+        while (nav != null)
+        {
+            if (i == n)
+                return nav.Value;
+            i++;
+            nav = nav.Next;
+        }
+        return 0;
+    }
+}
