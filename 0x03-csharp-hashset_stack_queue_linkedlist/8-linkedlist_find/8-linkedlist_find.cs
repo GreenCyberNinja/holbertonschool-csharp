228374d74d93815c601c4dd326f3a1b3ac57f281
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-15 23:28:51 -0500

    finds specific value in linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int ans = 0;
+        foreach ( var item in myLList)
+        {
+            ans++;
+            if (item == value)
+                return ans - 1;
+        }
+        ans = -1;
+        return ans;
+    }
+}
