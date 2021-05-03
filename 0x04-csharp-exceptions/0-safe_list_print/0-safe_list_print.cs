a966c5b7a0e1f6df46fb3d74547f2293028117b2
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-02 21:57:32 -0500

    readme directory and the first project

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int num= 0;
+        for (int i = 0; i < n; i++)
+        {
+            try
+            {
+                Console.WriteLine(myList[i]);
+                num++;
+            }
+            catch
+        }
+        return j;
+    }
+}
