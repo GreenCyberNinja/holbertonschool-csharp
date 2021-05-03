5ce9233e955d56b0e10528968129f32ee11b9326
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-02 22:02:07 -0500

    fixed num init statemtent

--- 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -5,7 +5,7 @@ class List
 {
     public static int SafePrint(List<int> myList, int n)
     {
-        int num= 0;
+        int num = 0;
         for (int i = 0; i < n; i++)
         {
             try
