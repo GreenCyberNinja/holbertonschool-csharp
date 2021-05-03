e90c9f17287b1b4a3cf028d216b12462e743374a
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-03 15:20:25 -0500

    updated with xml documentation

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -2,8 +2,10 @@
 
 class Obj
 {
+    ///<summary>class obj</summary>
     public static bool IsOfTypeInt(object obj)
     {
+        ///<summary>checks if obj is type int </summary>
         return (obj is int);
     }
 }
