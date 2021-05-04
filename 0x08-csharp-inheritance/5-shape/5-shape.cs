0cb31d056f50b0c0b728f8a1d1af39636afa95f1
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-03 20:25:18 -0500

    task 5 establish base class Shape

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>class Shape</summary>
+public class Shape
+{
+    ///<summary>public method area: currently doesn't work </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
