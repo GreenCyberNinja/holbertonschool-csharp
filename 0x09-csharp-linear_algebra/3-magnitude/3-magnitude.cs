3e7a55b2c7d5b9700d39b1a43e19a0d849bfe10e
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-25 22:20:16 -0500

    updated magnitude calculations

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using Math;
+using System.Collections.Generic;
+///<summary>does vector math</summary>
 class VectorMath
 {
+    ///<summary>calculate magnitude for 2d and 3d vectors</summary>
     public static double Magnitude(double[] vector)
     {
-        if (vector.Length() < 2 || vector.Length() > 4)
+        if (vector.Length < 1 || vector.Length > 3)
             return -1;
-        double Asqr = Round(vector[1] * vector[1], 2);
-        double Bsqr = Round(vector[2] *vector[2], 2);
-        Console.WriteLine(Asqr);
-        Console.WriteLine(Bsqr);
-        return (vector[1]*vector);
+        double Asqr = vector[0] * vector[0];
+        double Bsqr = vector[1] *vector[1];
+        if (vector.Length > 2)
+        {
+            double Csqr = vector[2] * vector[2];
+            return (Math.Round(Math.Sqrt(Asqr+Bsqr+Csqr), 2));
+        }
+        return (Math.Round(Math.Sqrt(Asqr+Bsqr), 2));
     }
 }
