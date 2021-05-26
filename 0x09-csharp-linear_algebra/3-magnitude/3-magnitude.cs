b81c053975bb24177339d250fdc9067e727b0ea8
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-25 21:12:42 -0500

    quick push to get main file for testing

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Math;
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length() < 2 || vector.Length() > 4)
+            return -1;
+        double Asqr = Round(vector[1] * vector[1], 2);
+        double Bsqr = Round(vector[2] *vector[2], 2);
+        Console.WriteLine(Asqr);
+        Console.WriteLine(Bsqr);
+        return (vector[1]*vector);
+    }
+}
