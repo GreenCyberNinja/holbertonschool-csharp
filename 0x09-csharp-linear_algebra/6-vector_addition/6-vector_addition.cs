d3e984064eb43af46faca2deb25504f4c1c0a74a
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-26 00:38:02 -0500

    first commit

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double[] ans = new double[3];
+        if (vector1.Length < 2 || vector1.Length > 3)
+            return (ans.SetValue(-1, 0));
+            
+        if (vector2.Length < 2 || vector2.Length > 3)
+            return (ans.SetValue(-1, 0));
+        if (vector1.Length != vector2.Length)
+            return (ans.SetValue(-1, 0));
+        for (int i = 0; i <= vector1.Length;i++)
+        {
+            ans.SetValue(vector1[i]+vector2[i], i);
+        }
+        return ans;
+    }
+}
