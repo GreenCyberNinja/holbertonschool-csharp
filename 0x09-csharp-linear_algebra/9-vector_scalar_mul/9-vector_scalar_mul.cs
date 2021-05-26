9255743e439a320899387f93176b17bb7a57f2db
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-26 01:57:21 -0500

    multiplies vector by scalar

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        List<double> anslist = new List<double>();
+        double[] ans = new double[3];
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            ans.SetValue(-1, 0);
+            return ans;
+        }
+        for (int i = 0; i < vector.Length;i++)
+        {
+            anslist.Add(vector[i]*scalar);           
+        }
+        ans = anslist.ToArray();
+        return ans;
+    }
+}
