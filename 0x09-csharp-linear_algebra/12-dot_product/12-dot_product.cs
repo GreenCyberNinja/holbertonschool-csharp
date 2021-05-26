668ff21083d2cc1309738f42e2de160a0f44eec2
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-26 15:37:43 -0500

    calculates dot product

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>does vector math</summary>
+class VectorMath
+{
+    ///<summary>dot product</summary>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        double ans;
+        if (vector1.Length < 2 || vector1.Length > 3)
+        {
+            ans = -1;
+            return ans;
+        }   
+        if (vector2.Length < 2 || vector2.Length > 3)
+        {
+            ans = -1;
+            return ans;
+        }
+        if (vector1.Length != vector2.Length)
+        {
+            ans = -1;
+            return ans;
+        }
+        for (int i = 0; i < vector1.Length;i++)
+        {
+            ans += vector1 * vector2;           
+        }
+        return ans;
+    }
+}
