84888e1a94bcf3b248918c5687bd82a74d75411d
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-26 00:56:37 -0500

    adds two vectors together

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 
+///<summary>does vector math</summary>
 class VectorMath
 {
+    ///<summary>adds two vectors together</summary>
     public static double[] Add(double[] vector1, double[] vector2)
     {
+        List<double> anslist = new List<double>();
         double[] ans = new double[3];
         if (vector1.Length < 2 || vector1.Length > 3)
-            return (ans.SetValue(-1, 0));
-            
-        if (vector2.Length < 2 || vector2.Length > 3)
-            return (ans.SetValue(-1, 0));
-        if (vector1.Length != vector2.Length)
-            return (ans.SetValue(-1, 0));
-        for (int i = 0; i <= vector1.Length;i++)
         {
-            ans.SetValue(vector1[i]+vector2[i], i);
+            ans.SetValue(-1, 0);
+            return ans;
+        }   
+        if (vector2.Length < 2 || vector2.Length > 3)
+        {
+            ans.SetValue(-1, 0);
+            return ans;
         }
+        if (vector1.Length != vector2.Length)
+        {
+            ans.SetValue(-1, 0);
+            return ans;
+        }
+        for (int i = 0; i < vector1.Length;i++)
+        {
+            anslist.Add(vector1[i]+vector2[i]);           
+        }
+        ans = anslist.ToArray();
         return ans;
     }
 }
