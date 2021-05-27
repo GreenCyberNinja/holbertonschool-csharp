8dd7b22aced154b7c671a1e8be4212fc620e94e0
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-26 22:40:33 -0500

    multiplies matrix by scalar

---
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+///<summary>math done to a matrix</summary>
+class MatrixMath
+{
+    ///<summary>multiplies matrix by scalar</summary>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        double[,] wrng = {{-1}};
+        double[,] ans;
+        int chk;
+
+        if (matrix.Length == 4)
+        {
+            chk = 2;
+            ans = new double[2, 2];
+        }
+        else
+        {
+            chk = 3;
+            ans = new double[3,3];
+        }
+
+        if (matrix.Length < 4 || matrix.Length > 9)
+            return wrng;
+        for (int i=0; i < chk; i++)
+        {
+            for (int j=0; j < chk; j++)
+            {
+                ans[i,j] = matrix[i,j] * scalar;
+            }
+        }
+        return ans;
+    }
+}
